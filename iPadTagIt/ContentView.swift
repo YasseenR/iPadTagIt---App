392bdbca1f73c7b62b5e6030b231ecab60a34764
YasseenR <114093367+YasseenR@users.noreply.github.com>
2025-09-03 12:21:40 -0400

    Initial Commit

---
+++ iPadTagIt/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iPadTagIt
+//
+//  Created by Yasseen Rouni on 9/3/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iPadTagIt/iPadTagItApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPadTagItApp.swift
+//  iPadTagIt
+//
+//  Created by Yasseen Rouni on 9/3/25.
+//
+
+import SwiftUI
+
+@main
+struct iPadTagItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
