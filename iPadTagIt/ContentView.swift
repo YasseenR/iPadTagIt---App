d9ae0f067dadc2c3c40a7991f7f54a58489d8371
YasseenR <114093367+YasseenR@users.noreply.github.com>
2025-09-08 20:07:58 -0400

    Added qr code scan functionality

--- iPadTagIt/ContentView.swift
+++ iPadTagIt/ContentView.swift
@@ -5,17 +5,43 @@
 //  Created by Yasseen Rouni on 9/3/25.
 //
 
+import CodeScanner
 import SwiftUI
 
 struct ContentView: View {
+    @State private var assetTag = ""
+    @State private var isShowingScanner = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            List {
+                HStack {
+                    TextField("Enter Asset Tag", text: $assetTag)
+                        .frame(height: 50)
+                }
+            }
+            Button(action: {
+                isShowingScanner = true
+            }) {
+                Image(systemName: "qrcode.viewfinder")
+            }
+            .sheet(isPresented: $isShowingScanner) {
+                CodeScannerView(codeTypes: [.qr], simulatedData: "CPH10718", completion: handleScan)
+            }
         }
-        .padding()
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        switch result {
+        case .success(let result):
+            let details = result.string
+            
+            assetTag = details
+        
+            
+        case .failure(let error):
+            print("Scaning failed: \(error.localizedDescription)")
+        }
+        print("Handled Scan with asset tag: \(assetTag)")
     }
 }
 
